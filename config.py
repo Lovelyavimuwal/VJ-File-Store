8071fef75d034377d44db5482a04f8ea5fca256f
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-05-29 02:51:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://hurt-falcon-lovelyavimuwal-909f06b5.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://hurt-falcon-lovelyavimuwal-909f06b5.koyeb.app/"
  # Url Shortner Information 
     TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
     TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
